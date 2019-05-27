e2452aeff62539e763ec17ce4faf2a184b8610a5
ra184-2014 <stanee95@live.com>
2019-05-27 22:50:53 +0200

    Dovršena gramatika

--- hl7test.py
+++ hl7test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from os.path import join, dirname
+
+from builtins import print
 from textx import metamodel_from_file
 from textx.export import metamodel_export, model_export
 
@@ -9,17 +11,18 @@ def main(debug=False):
     this_folder = dirname(__file__)
 
     # Get meta-model from language description
-    hello_meta = metamodel_from_file(join(this_folder, 'grammar.tx'), debug=debug, skipws=True, ws='\s')
+    meta_model = metamodel_from_file(join(this_folder, 'grammar.tx'), debug=debug, skipws=True, ws='\s')
 
     # Optionally export meta-model to dot
-    metamodel_export(hello_meta, join(this_folder, 'hl7model.dot'))
+    metamodel_export(meta_model, join(this_folder, 'hl7model.dot'))
 
     # Instantiate model
-    example_hello_model = hello_meta.model_from_file(join(this_folder, 'example.txt'))
+    model = meta_model.model_from_file(join(this_folder, 'example.txt'))
 
     # Optionally export model to dot
-    model_export(example_hello_model, join(this_folder, 'example.dot'))
+    model_export(model, join(this_folder, 'example.dot'))
 
+    print(model.start)
 
 if __name__ == '__main__':
     main()
